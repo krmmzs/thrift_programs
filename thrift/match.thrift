8896a86a5250f37a5021bfd5277bbd3875d000a4
mouzaisi <mouzaisibyl@gmail.com>
2021-10-01 17:04:01 +0800

    add match_thrift comments

--- thrift/match.thrift
+++ thrift/match.thrift
@@ -1,4 +1,4 @@
-namespace cpp match_service
+mespace cpp match_service
 
 struct User
 {
@@ -9,7 +9,19 @@ struct User
 
 service Match
 {
+
+    /**
+     * user: 添加的用户信息
+     * info: 附加信息
+     * 在匹配池中添加一个名用户
+     */
     i32 add_user(1: User user, 2: string info),
 
+    /**
+     * user: 删除的用户信息
+     * info: 附加信息
+     * 从匹配池中删除一名用户
+     */
     i32 remove_user(1: User user, 2: string info),
 }
+
